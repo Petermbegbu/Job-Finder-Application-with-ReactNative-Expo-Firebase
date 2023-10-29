23c508d35f76647b541dea7ed81f60d61bd8ab3f
Petermbegbu <petermbegbu@gmail.com>
2023-10-29 21:14:58 +0000

    Created empty pages, components and file templates

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, SIZES, icons, images } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: true,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
